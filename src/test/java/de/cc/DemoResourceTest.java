a2ec9348bed87a588a6d384eb511904cb1b59a2d
seb1ast8ian0 <s.staedler05@gmail.com>
2024-01-03 14:56:59 +0100

    Initial commit

---
+++ src/test/java/de/cc/DemoResourceTest.java
@@ -0,0 +1,20 @@
+package de.cc;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class DemoResourceTest {
+    @Test
+    void testDemoEndpoint() {
+        given()
+                .when().get("/demo")
+                .then()
+                .statusCode(200)
+                .body(is("Hello World!"));
+    }
+
+}

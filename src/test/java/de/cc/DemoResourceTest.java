3d4333d1b2f0f66b62bb293638e4abf09bd35eb5
seb1ast8ian0 <s.staedler05@gmail.com>
2024-01-12 08:13:52 +0100

    change Text

--- src/test/java/de/cc/DemoResourceTest.java
+++ src/test/java/de/cc/DemoResourceTest.java
@@ -14,7 +14,7 @@ class DemoResourceTest {
                 .when().get("/demo")
                 .then()
                 .statusCode(200)
-                .body(is("Hello World 2!!"));
+                .body(is("Hello World!"));
     }
 
 }
